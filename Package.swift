ee49daded643dbbfb13c4627d641579a5c216dcd
Hüseyin <huseyinbagana@yandex.com>
2021-01-05 08:23:58 +0300

    json file included.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TransactionAlert",
-            dependencies: ["Lottie","PartialSheet"]),
+            dependencies: ["Lottie","PartialSheet"],
+            resources: [.copy("taAnimation.json")]),
         .testTarget(
             name: "TransactionAlertTests",
             dependencies: ["TransactionAlert"]),
